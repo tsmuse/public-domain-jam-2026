99cbb80600db8f2f2f1104ba63175587f89bb465
Tony Santos <tony@notjustprettypictures.com>
2026-01-31 11:23:34 -0800

    fixes update issues with beat warning, added level complete message to beat warning bar

--- Levels/3p_test_level.gd
+++ Levels/3p_test_level.gd
@@ -14,6 +14,7 @@ extends Node2D
 @onready var beat_warning_2 := %Beat2
 @onready var beat_warning_3 := %Beat3
 @onready var beat_warning_4 := %Beat4
+@onready var success_text := %SuccessText
 @onready var composer_resolve_bar := %ComposerResolve
 
 @export var scene_number := 0
@@ -44,6 +45,7 @@ var static_monitoring := true
 
 var measure_label_string := "Measure %s of %s"
 var beat_warning_string_safe := "%s[font_size=\"32px\"]&[/font_size]"
+var beat_warning_string_complete := "[color=aqua]%s[font_size=\"32px\"]&[/font_size][/color]"
 var beat_warning_string_warn := "[shake rate=20.0 level=15 connected=1][color=crimson]%s[font_size=\"32px\"]&[/font_size][/color][/shake]"
 
 
@@ -80,18 +82,31 @@ func _process(_delta):
 		current_measure_complete = false
 		despair_should_grow = false
 		despair_timer.stop()
+		despair.clear()
+		beat_warning_1.visible = false
+		beat_warning_2.visible = false
+		beat_warning_3.visible = false
+		beat_warning_4.visible = false
+		success_text.visible = true
+		
 		next_level_pad.visible = true
 		
 	if current_measure_complete:
 		print("measure complete")
 		print("current_measure before update: %s" % current_measure)
 		current_measure += 1
+		beat_warning_1.text = beat_warning_string_complete % 1
+		beat_warning_2.text = beat_warning_string_complete % 2
+		beat_warning_3.text = beat_warning_string_complete % 3
+		beat_warning_4.text = beat_warning_string_complete % 4
+
 		if current_measure == measures.get_children().size():
 			level_complete = true
 		else:
 			current_measure_complete = false
 			_set_current_bar()
 			_start_notes_in_current_bar()
+
 	
 	if not current_measure_complete:
 		# print("measure not complete")
@@ -132,6 +147,15 @@ func _process(_delta):
 					beat_warning_3.text = beat_warning_string_safe % current_note.beat
 				elif current_note.beat == 4:
 					beat_warning_4.text = beat_warning_string_safe % current_note.beat
+			elif current_note.note_complete:
+				if current_note.beat == 1:
+					beat_warning_1.text = beat_warning_string_complete % current_note.beat
+				elif current_note.beat == 2:
+					beat_warning_2.text = beat_warning_string_complete % current_note.beat
+				elif current_note.beat == 3:
+					beat_warning_3.text = beat_warning_string_complete % current_note.beat
+				elif current_note.beat == 4:
+					beat_warning_4.text = beat_warning_string_complete % current_note.beat
 
 
 		# draw the despair tiles
@@ -155,28 +179,6 @@ func _process(_delta):
 		static_monitoring = false
 	
 	despair_count = despair.get_used_cells().size()
-	
-	# enable these if you're actually changing them while the game it running
-	#speed_label.text = "Max Speed: %s" % controller.max_speed
-	#acel_label.text = "Max Acel: %s" % controller.max_accel
-	#decel_label.text = "Max Decl: %s" % controller.max_decel
-	#turn_label.text = "Turn Speed: %s" % controller.turn_speed
-	#
-	#jumph_label.text = "Jump Height: %s px" % controller.jump_height
-	#jumpd_label.text = "Jump Duration: %s sec" % controller.jump_duration
-	#downg_label.text = "Down Gravity: %s x G" % controller.down_gravity
-	#ab_label.text = "Air Break: %s" % controller.air_brake
-	#aa_label.text = "Air Accel: %s" % controller.max_air_accel
-	#ac_label.text = "Air Control: %s" % controller.air_control
-	#vh_label.text = "Variable Height Jumps: %s" % controller.variable_height
-	#vhcut_label.text = "Variable Height Cutoff: %s" % controller.variable_height_cutoff
-	#airjump_label.text = "Air Jumps: %s" % controller.air_jump
-	#airjumpcount_label.text = "Concurent Air Jumps: %s" % controller.concurent_air_jumps
-	#
-	#coyote_label.text = "Coyote Time: %s" % controller.coyote_time
-	#jumpb_label.text = "Jump Buffer: %s" % controller.jump_buffer
-	#tv_label.text = "Terminal Velocity: %s x G" % controller.terminal_velocity
-	
 
 func _physics_process(_delta: float) -> void:
 	if player.despair_detector.has_overlapping_bodies():
@@ -237,6 +239,10 @@ func _set_current_bar() -> void:
 	despair_start_coord = bar.despair_rect_start
 	despair_rect_size = Vector2i(bar.despair_rect_length, bar.despair_rect_height)
 	despair_rects_drawn = 0
+	beat_warning_1.text = beat_warning_string_safe % 1
+	beat_warning_2.text = beat_warning_string_safe % 2
+	beat_warning_3.text = beat_warning_string_safe % 3
+	beat_warning_4.text = beat_warning_string_safe % 4
 
 func _on_despair_timer_timeout() -> void:
 	despair_should_grow = true
